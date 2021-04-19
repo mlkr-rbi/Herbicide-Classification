b5fbf0ee24e1b84fe14234e2468318b61c6d8006
davoors <75166378+davoors@users.noreply.github.com>
2021-04-19 21:24:46 +0200

    Create xgb_classifier.R

---
+++ HRAC/models/xgb_classifier.R
@@ -0,0 +1,32 @@
+# Set trainControl
+xgb.ctrl <- trainControl(method = "repeatedcv", repeats = 10, number = 10,  returnResamp = "final", savePredictions = "final") # , allowParallel=T
+
+# Set up a tuning grid
+xgb.grid <- expand.grid(nrounds = c(100, 500, 1000, 1500), 
+                        eta = c(0.01, 0.05, 0.1, 0.3), 
+                        max_depth = c(4,6,10, 14),
+                        gamma = 0,
+                        colsample_bytree = 0.5,
+                        min_child_weight = 1,
+                        subsample = 1)
+
+# Hyperparameters tuning
+set.seed(111)
+hrac_xgb <- train(factor(HRAC2020_class) ~., data = h.train,
+                  method="xgbTree",
+                  trControl=xgb.ctrl,
+                  tuneGrid=xgb.grid,
+                  metric="Accuracy")
+
+plot(hrac_xgb)
+
+
+# Train model on on whole train set with optimal hyperparameters
+set.seed(111)
+hrac_xgb.fin <- train(factor(HRAC2020_class) ~., data = h.train,
+                      method="xgbTree",
+                      trControl=trainControl(method="none"),
+                      tuneGrid=hrac_xgb$bestTune, 
+                      metric="Accuracy")
+
+saveRDS(hrac_xgb.fin, "./hrac_xgb_model.rds")
