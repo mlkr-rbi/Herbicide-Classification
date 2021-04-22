7b1400cf75db0d50bbdf227b5c34686459430e24
davoors <75166378+davoors@users.noreply.github.com>
2021-04-22 13:33:22 +0200

    Update nb_classifier.R

--- HRAC/models/nb_classifier.R
+++ HRAC/models/nb_classifier.R
@@ -1,19 +1,19 @@
 library(klaR)
 
 # Set trainControl
-nb.ctrl <- trainControl(method="repeatedcv", number=10, repeats=10, returnResamp = "final", savePredictions = "final") # , sampling = "up"
+nb.ctrl <- trainControl(method="repeatedcv", number=10, repeats=10, returnResamp = "final", savePredictions = "final")
 
 # Set up a tuning grid
-grid <-  expand.grid(fL=c(0,0.3,0.5,0.7,1.0), 
-                     usekernel = TRUE, 
-                     adjust=c(0.1,0.3,0.5,0.7,1.0))
+nb.grid <-  expand.grid(fL=c(0,0.3,0.5,0.7,1.0), 
+                        usekernel = TRUE, 
+                        adjust=c(0.1,0.3,0.5,0.7,1.0))
 
 
 # Hyperparameters tuning
 set.seed(111)
 hrac_nb <- train(factor(HRAC2020_class) ~., data = h.train, 
                  method = 'nb',
-                 tuneGrid = grid,
+                 tuneGrid = nb.grid,
                  trControl=nb.ctrl,
                  metric = "Accuracy")
 
