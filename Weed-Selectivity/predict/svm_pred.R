51f12c689d1f18ecfaf468b954add3319171fb6b
davoors <75166378+davoors@users.noreply.github.com>
2021-04-22 14:01:49 +0200

    Create svm_pred.R

---
+++ Weed-Selectivity/predict/svm_pred.R
@@ -0,0 +1,40 @@
+# Import pretrained model
+selD_svm.fin <- readRDS("./selD_svm.fin")
+selP_svm.fin <- readRDS("./selP_svm.fin")
+
+
+# A) Predict for each dataset - "label"
+sSVM.pred.ls <- list(LogD=list(), LogP=list()) 
+
+for (i in 1:4) {
+   sSVM.pred.ls[["LogD"]][[i]] <- predict(selD_svm.fin, sel_ts.ls[["LogD"]][[i]][,-1])
+   sSVM.pred.ls[["LogP"]][[i]] <- predict(selP_svm.fin, sel_ts.ls[["LogP"]][[i]][,-1])
+
+    names(sSVM.pred.ls[["LogD"]])[i] <- names(sel_ts.ls[["LogD"]])[i]
+    names(sSVM.pred.ls[["LogP"]])[i] <- names(sel_ts.ls[["LogP"]])[i]
+}
+          
+          
+# B) Predict for each dataset - "prob"
+sSVM.pred_p.ls <- list(LogD=list(), LogP=list())
+          
+for (i in 1:4) {
+    sSVM.pred_p.ls[["LogD"]][[i]] <- predict(selD_svm.fin, sel_ts.ls[["LogD"]][[i]][,-1], "prob")
+    sSVM.pred_p.ls[["LogP"]][[i]] <- predict(selP_svm.fin, sel_ts.ls[["LogP"]][[i]][,-1], "prob")
+
+    names(sSVM.pred_p.ls[["LogD"]])[i] <- names(sel_ts.ls[["LogD"]])[i]
+    names(sSVM.pred_p.ls[["LogP"]])[i] <- names(sel_ts.ls[["LogP"]])[i]
+
+}
+          
+# C) Confusion matrix
+sSVM.conf.ls <- list(LogD=list(), LogP=list())
+          
+for (i in 1:2) {
+    sSVM.conf.ls[["LogD"]][[i]] <- confusionMatrix(sSVM.pred.ls[["LogD"]][[i]], factor(sel_ts.ls[["LogD"]][[i]]$Selectivity))
+    sSVM.conf.ls[["LogP"]][[i]] <- confusionMatrix(sSVM.pred.ls[["LogP"]][[i]], factor(sel_ts.ls[["LogP"]][[i]]$Selectivity))
+            
+    names(sSVM.conf.ls[["LogD"]])[i] <- names(sel_ts.ls[["LogD"]])[i]
+    names(sSVM.conf.ls[["LogP"]])[i] <- names(sel_ts.ls[["LogP"]])[i]
+
+}
